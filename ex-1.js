feccd149e57c4fbcb5fce3179c7ae9b00bdcbbd6
Eisss20 <pajenan.j@gmail.com>
2024-10-14 23:10:52 +0700

    Feat : Add answer assigement

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,41 @@
 // Exercise #1: For Each Function
 
-function forEach(array, operation) {
-  // Start coding here
-}
-
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
+function ForEach (array,callback) {
+for (let salary of array) {
+newEmployeeSalaries.push(callback(salary));
+} 
+}
+
+let addSalaries = (salary) => salary + 5000;
+
 // Using `forEach` function here
 
+ForEach(employeeSalaries, addSalaries);
+
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+
+
+
+
+
+
+
+// const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
+// const newEmployeeSalaries = [];
+
+
+// function forEach(array, operation) {
+// for (let salaryAdd of array ) {
+//   operation.push(salaryAdd+5000)
+// }
+// return operation
+// }
+
+
+// // Using `forEach` function here
+// forEach(employeeSalaries,newEmployeeSalaries)
+// console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+
--- ex-2.js
+++ ex-2.js
@@ -1,8 +1,18 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+
+let CheckqtyOfScor = 0; 
+for (let score of array ) {
+
+    if (operation(score)) {
+    CheckqtyOfScor++   
+    }
 }
+return CheckqtyOfScor >=5 ;
+}
+
+let checkScores = (score) => score > 70;
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
@@ -10,6 +20,58 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkScores);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkScores);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkScores);
+
+console.log(`นักเรียนห้องที่ 1 ${scoreRoom1Result ? "ผ่านเกณฑ์ ✅" : "ไม่ผ่านเกณฑ์ ❌"}`);
+console.log(`นักเรียนห้องที่ 2 ${scoreRoom2Result ? "ผ่านเกณฑ์ ✅" : "ไม่ผ่านเกณฑ์ ❌"}`);
+console.log(`นักเรียนห้องที่ 3 ${scoreRoom3Result ? "ผ่านเกณฑ์ ✅" : "ไม่ผ่านเกณฑ์ ❌"}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Exercise #2: At Least Five Function
+
+// function atLeastFive(array, operation) {
+//     // Start coding here
+//     let count = 0;
+//     for (let i = 0; i < array.length; i++) {
+//       if (operation(array[i])) {
+//         count++;
+//       }
+//     }
+//     return count >= 5;
+//   }
+  
+//   function checkGradeOver70(score) {
+//     return score > 70;
+//   }
+  
+//   const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
+//   const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
+//   const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
+  
+//   // Using `atLeastFive` function here
+  
+//   let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkGradeOver70);
+//   let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkGradeOver70);
+//   let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkGradeOver70);
+  
+//   console.log(scoreRoom1Result);
+//   console.log(scoreRoom2Result);
+//   console.log(scoreRoom3Result);
+  
